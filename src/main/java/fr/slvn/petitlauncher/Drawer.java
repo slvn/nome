20f46fad7a8d0760b8a45e28dde126dcc011f545
Sylvain Galand <sylvain.galand@gmail.com>
2013-09-30 22:24:24 +0200

    Add a Drawer activity launch when clicking Home

---
+++ src/main/java/fr/slvn/petitlauncher/Drawer.java
@@ -0,0 +1,6 @@
+package fr.slvn.petitlauncher;
+
+import android.app.Activity;
+
+public class Drawer extends Activity {
+}
--- src/main/java/fr/slvn/petitlauncher/Home.java
+++ src/main/java/fr/slvn/petitlauncher/Home.java
@@ -1,10 +1,18 @@
 package fr.slvn.petitlauncher;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 
-/**
- *
- */
 public class Home extends Activity {
 
+    private static final String TAG = "Home";
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d(TAG, "[onNewIntent]");
+        // Pretty sure we need to launch the Drawer here !
+        startActivity(new Intent(this, Drawer.class));
+    }
 }
