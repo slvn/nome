2887d7d0329642480de9bb30a89ab69a23ae2a43
BoD <BoD@JRAF.org>
2013-10-15 21:54:29 +0200

    Settings activity, and setting for showing the wallpaper.

--- src/main/java/fr/slvn/petitlauncher/Drawer.java
+++ src/main/java/fr/slvn/petitlauncher/Drawer.java
@@ -4,9 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+
+import fr.slvn.petitlauncher.settings.SettingsActivity;
 
 public class Drawer extends Activity {
 
@@ -29,5 +34,29 @@ public class Drawer extends Activity {
                 startActivity(intent);
             }
         });
+
+        boolean showWallPaper = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(SettingsActivity.PREF_SHOW_WALLPAPER,
+                SettingsActivity.PREF_SHOW_WALLPAPER_DEFAULT);
+        if (showWallPaper) {
+            gridView.setBackground(null);
+        } else {
+            gridView.setBackgroundResource(android.R.color.background_dark);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
